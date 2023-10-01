123c63ede44ad009230e01042c3ba4b815aa61bb
Gaangb <angelob4stos@gmail.com>
2023-10-01 20:26:46 -0300

    feat: Adicionando receitas na pagina home

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,29 +1,79 @@
-import { useState } from 'react'
-import axios from 'axios'
-import image from '../../assets/home_image.webp'
-import styles from './home.module.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import image from "../../assets/home_image.webp";
+import styles from "./home.module.css";
+import { RecipeCard } from "../../components/molecules/recipeCard";
 
-export function Home(){
-      // const [recipe, setRecipe] = useState("")
+type Meal = {
+  mealIngredientes: Array<string>;
+  mealName: string;
+  mealImage: string;
+};
 
-  async function getRecipe(){
+export function Home() {
+  const [meal, setMeal] = useState<Meal>({
+    mealName: "",
+    mealImage: "",
+    mealIngredientes: [],
+  });
+
+  useEffect(() => {
+    getRecipe();
+  },[]);
+
+  async function getRecipe() {
     try {
-      const response = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php")
-      console.log(response.data)
-      // setRecipe(response)
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+      );
+
+      const mealData = response.data.meals[0];
+      setMeal((state) => ({
+        ...state,
+        mealName: mealData.strMeal,
+        mealImage: mealData.strMealThumb,
+        mealIngredientes: [
+          mealData.strIngredient1,
+          mealData.strIngredient2,
+          mealData.strIngredient3,
+          mealData.strIngredient4,
+          mealData.strIngredient5,
+          mealData.strIngredient6,
+          mealData.strIngredient7,
+          mealData.strIngredient8,
+          mealData.strIngredient9,
+          mealData.strIngredient10,
+          mealData.strIngredient11,
+          mealData.strIngredient12,
+          mealData.strIngredient13,
+          mealData.strIngredient14,
+          mealData.strIngredient15,
+          mealData.strIngredient16,
+          mealData.strIngredient17,
+          mealData.strIngredient18,
+          mealData.strIngredient19,
+          mealData.strIngredient20,
+        ],
+      }));
     } catch (error) {
-      console.log("erro")
+      console.log("erro");
     }
   }
 
-    return(
-        <div className={styles.container_home}>
-            <div >
-                <p>teste</p>
-            </div>
-            <div>
-                <img src={image} />
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container_home}>
+      <div className={styles.card_home}>
+        <h2>O que vamos comer hoje?</h2>
+        <p>Recomendação do momento</p>
+        <RecipeCard
+          image={meal.mealImage}
+          titulo={meal.mealName}
+          ingredientes={meal.mealIngredientes}
+        />
+      </div>
+      <div>
+        <img src={image} />
+      </div>
+    </div>
+  );
 }
