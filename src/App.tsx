09f4077458517df478c6b6b365375d95aa8dbaa3
Gaangb <angelob4stos@gmail.com>
2023-10-01 20:28:16 -0300

    feat: retirando o try do app para adicionar no Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { useState } from 'react'
 import './App.css'
-import axios from 'axios'
 import {NavBar} from './components/molecules/navBar'
+import { Home } from './screens/home'
 
 function App() {
-  // const [recipe, setRecipe] = useState("")
-
-  async function getRecipe(){
-    try {
-      const response = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php")
-      console.log(response.data)
-      // setRecipe(response)
-    } catch (error) {
-      console.log("erro")
-    }
-  }
 
   return (
     <>
       <NavBar />
+      <Home />
     </>
   )
 }
