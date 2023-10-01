e8b6f2e42e4a1a16adc42d097b618e3ba45b128a
Gaangb <angelob4stos@gmail.com>
2023-10-01 16:50:02 -0300

    feat: adicionando arquitetura Atomic Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [recipe, setRecipe] = useState("")
+
+  async function getRecipe(){
+    try {
+      const response = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php")
+      console.log(response.data)
+      // setRecipe(response)
+    } catch (error) {
+      console.log("erro")
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {getRecipe()}
     </>
   )
 }
