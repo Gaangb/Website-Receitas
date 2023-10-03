ccf290b28fdf8249ae0b0e5b1c0b976ce0379f3c
Gaangb <angelob4stos@gmail.com>
2023-10-03 12:15:15 -0300

    feat: iniciando logica na pagina de busca por nome

--- src/screens/searchName/index.tsx
+++ src/screens/searchName/index.tsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Input } from '../../components/atoms/input'
 import styles from './searchName.module.css'
 import image from '../../assets/search_name_image.webp'
 
 export function SearchName(){
+    const [recipeName, setRecipeName] = useState('');
+    const [recipesFinded, setRecipesFinded] = useState([])
+
+    async function handleSearch(){
+        try {
+            const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName}`)
+            setRecipesFinded(response.data.meals)
+            console.log(recipesFinded);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        handleSearch();
+      },[recipeName]);
+
     return(
         <div className={styles.container_geral_search_name}>
             <div className={styles.container_search_name}>
@@ -10,8 +29,13 @@ export function SearchName(){
                     <h2>Pesquisar receitas por nome:</h2>
                 </div>
                 <div>
-                    <Input text="Digite sua pesquisa" customClass="input_search_name"/>
+                    <Input text="Digite sua pesquisa" customClass="input_search_name" onChange={(e) => setRecipeName(e.target.value)}/>
                 </div>
+                {recipesFinded.map((recipe: any) => (
+                    <div key={recipe.idMeal}>
+                        <p>{recipe.strMeal}</p>
+                    </div>
+                ))}
             </div>
             <div>
                 <img src={image} alt="" />
