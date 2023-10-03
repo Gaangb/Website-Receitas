85f9b8a55e4c3e681f43c0f7c90797bba888c02f
Gaangb <angelob4stos@gmail.com>
2023-10-03 12:13:59 -0300

    feat: adicionando icone de lupa no input

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -1,12 +1,19 @@
 import styles from "./input.module.css";
+import { BsSearchHeart } from "react-icons/bs";
 
 interface InterfaceProps {
   text: string;
   customClass?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ text, customClass }: InterfaceProps) {
+export function Input({ text, customClass, onChange }: InterfaceProps) {
     const classNames = customClass ? `${styles[customClass]}` : "";
   
-    return <input type="text" placeholder={text} className={classNames} />;
+    return (
+        <div className={styles.container_input}>
+            <input type="text" placeholder={text} className={classNames} onChange={onChange}/>
+            <BsSearchHeart/>
+        </div>
+    )
 }
