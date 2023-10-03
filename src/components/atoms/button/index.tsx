17cc13d0b9672711b59bb9bff2f7a0b5d9ccda3d
Gaangb <angelob4stos@gmail.com>
2023-10-03 12:12:51 -0300

    feat: Adicionando navigate nos botões

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -3,8 +3,9 @@ import styles from "./button.module.css";
 interface ButtonProps {
   text: string;
   customClass?: string;
+  onClick?: () => void;
 }
 
-export function Button({ text, customClass }: ButtonProps) {
-  return <button className={`${styles}.${customClass}`}>{text}</button>;
+export function Button({ text, customClass, onClick }: ButtonProps) {
+  return <button onClick={onClick} className={`${styles}.${customClass}`}>{text}</button>;
 }
--- src/components/molecules/navBar/index.tsx
+++ src/components/molecules/navBar/index.tsx
@@ -1,15 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import styles from './navBar.module.css'
 import { Button } from "../../atoms/button"
 
 export function NavBar(){
+    const navigate = useNavigate()
+
+    function handleNavigateToHome(){
+        navigate('/');
+    }
+
+    function handleNavigateToSearchByName(){
+        navigate('/search-name');
+    }
+
     return(
         <div className={styles.container_NabBar}>
             <div>
                 <h1>Receitas BG</h1>
             </div>
             <div className={styles.container_buttons_navBar}>
-                <Button text="Home" customClass="Button_NavBar"/>
-                <Button text="Busca por nome" customClass="Button_NavBar"/>
+                <Button text="Home" customClass="Button_NavBar" onClick={() => handleNavigateToHome()}/>
+                <Button text="Busca por nome" customClass="Button_NavBar" onClick={() => handleNavigateToSearchByName()}/>
                 <Button text="Busca Alfabetica" customClass="Button_NavBar"/>
                 <Button text="Busca por ingredientes" customClass="Button_NavBar"/>
             </div>
