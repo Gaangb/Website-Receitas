2d55b755398481eed907b327457941ab7ee192cc
Gaangb <angelob4stos@gmail.com>
2023-10-03 22:21:55 -0300

    feat: criando card do search recipe card

---
+++ src/components/molecules/searchRecipeCard/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./searchRecipeCard.module.css";
+
+interface CardProps {
+  image: string;
+  titulo: string;
+  key?: number;
+
+}
+
+export function SearchRecipeCard({ image, titulo, key}: CardProps) {
+  return (
+    <div className={styles.container_search_recipe_card}>
+        <div>
+            <p title={titulo}>{titulo}</p>
+        </div>
+        <div className={styles.search_recipe_card_image}>
+            <img src={image}/>
+        </div>
+    </div>
+  );
+}
