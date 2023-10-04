528798d5644d7fba6a3e0c21be3108c241b64463
Gaangb <angelob4stos@gmail.com>
2023-10-04 17:30:23 -0300

    feat: criando pagina busca por ingrediente

---
+++ src/screens/searchByIngredient/index.tsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Input } from "../../components/atoms/input";
+import styles from "./searchByIngredient.module.css";
+import image from "../../assets/search_ingredient_image.webp";
+import { SearchRecipeCard } from "../../components/molecules/searchRecipeCard";
+import { Button } from "../../components/atoms/button";
+
+export function SearchByIngredient() {
+  const [recipeIngredient, setRecipeIngredient] = useState("");
+  const [recipesFinded, setRecipesFinded] = useState([]);
+  const [ingredientsFinded, setIngredientsFinded] =useState([])
+
+  async function handleSearch() {
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${recipeIngredient}`
+      );
+      setRecipesFinded(response.data.meals);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function  getIngredients() {
+    try {
+      const response = await axios.get(
+        'https://www.themealdb.com/api/json/v1/1/list.php?i=list'
+      );
+      setIngredientsFinded(response.data.meals)
+      console.log(ingredientsFinded)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    handleSearch();
+    getIngredients()
+  }, [recipeIngredient]);
+
+  return (
+    <div className={styles.container_geral_search_name}>
+      <div className={styles.container_search_name}>
+        <div>
+          <h2>Pesquisar receitas por ingrediente:</h2>
+        </div>
+        <div>
+          <Input
+            text="Digite sua pesquisa"
+            customClass="input_search_name"
+            onChange={(e) => setRecipeIngredient(e.target.value)}
+          />
+        </div>
+        <div className={styles.container_search_meals}>
+          {recipesFinded ? (
+            recipesFinded.map((recipe: any) => (
+              <SearchRecipeCard
+                titulo={recipe.strMeal}
+                image={recipe.strMealThumb}
+                key={recipe.idMeal}
+              />
+            ))
+          ) : (
+            // ingredientsFinded.map((ingredient: any) => (
+            //   <Button text={ingredient} onClick={() => setRecipeIngredient(ingredient)} />
+            // ))
+            // <p>Não foi encontrada nenhuma receita.</p>
+            <div className={styles.container_search_buttons} >
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+              <Button text='Chicken temperada' customClass="ingredient_button"/>
+            </div>
+
+          )}
+        </div>
+      </div>
+      <div>
+        <img src={image} alt="" />
+      </div>
+    </div>
+  );
+}
